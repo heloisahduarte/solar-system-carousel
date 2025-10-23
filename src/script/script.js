673e50ca23231f53ac0ee0730277e0882dd8f71d
heloisa duarte <heloisaa.sdp@gmail.com>
2025-10-23 14:16:35 -0300

    update

---
+++ src/script/script.js
@@ -0,0 +1,16 @@
+
+    var swiper = new Swiper(".swiper", {
+        loop: true,
+        grabCursor: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+      },
+
+       effect: 'coverflow',
+  coverflowEffect: {
+    rotate: 30,
+    slideShadows: false,
+  },
+    });
